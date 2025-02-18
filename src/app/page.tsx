2944e92b55d5813f928e26fd43ff302ffe87680c
Harris Alam <harrisalam3@gmail.com>
2025-02-18 15:42:07 +0000

    Tidy main Page.tsx file

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,24 +3,40 @@
 import { useState, useEffect } from "react";
 import Header from "./Header";
 import Image from "next/image";
+import HeaderImage from "@/app/HeaderImage";
+import AdvertSection from "@/app/AdvertSection";
+
 export default function Home() {
-  const [theme, setTheme] = useState("light");
+    const [theme, setTheme] = useState("light");
 
-  useEffect(() => {
-    document.documentElement.classList.toggle("dark", theme === "dark");
-  }, [theme]);
+    useEffect(() => {
+        document.documentElement.classList.toggle("dark", theme === "dark");
+    }, [theme]);
 
-  return (
-      <div className="hero-section min-h-screen flex flex-col justify-center bg-white dark:bg-gray-950 text-black dark:text-white transition-colors duration-300 px-4 sm:px-8 md:px-16">
-        <button
-            onClick={() => setTheme(theme === "light" ? "dark" : "light")}
-            className="absolute top-4 right-4 px-4 py-2 bg-gray-200 dark:bg-gray-800 rounded"
-        >
-          Toggle {theme === "light" ? "Dark" : "Light"} Mode
-        </button>
-          {/*<Image className="absolute top-0 left-4" src="/logo.svg" alt="Two Good Co. Logo" width={100} height={100} />*/}
+    return (
+        <div className="hero-section min-h-screen bg-white dark:bg-gray-950 text-black dark:text-white transition-colors duration-300 px-4 sm:px-8 md:px-16">
+            {/* Theme Toggle Button */}
+            <button
+                onClick={() => setTheme(theme === "light" ? "dark" : "light")}
+                className="absolute top-4 right-4 px-4 py-2 bg-gray-200 dark:bg-gray-800 rounded"
+            >
+                Toggle {theme === "light" ? "Dark" : "Light"} Mode
+            </button>
 
-          <Header />
-      </div>
-  );
+            {/* First Section with Header */}
+            <div className="mt-16 flex">
+                <Header />
+            </div>
+
+            {/* Second Section with Header Image */}
+            <div className="mt-16 flex flex-col items-center">
+                <HeaderImage />
+            </div>
+
+            {/* Third Section with Advert Images */}
+            <div className="mt-16 flex flex-col items-center">
+                <AdvertSection />
+            </div>
+        </div>
+    );
 }
