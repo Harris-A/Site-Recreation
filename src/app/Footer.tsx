9964384bb95dab7a3138422e0d1a5f2776889401
Harris Alam <harrisalam3@gmail.com>
2025-02-24 16:52:05 +0000

    apply footer dark mode styling

--- src/app/Footer.tsx
+++ src/app/Footer.tsx
@@ -2,10 +2,10 @@ import React from 'react';
 
 const Footer = () => {
     return (
-        <div className="footer border-t-2 border-black w-full text-center leading-8 pt-8 pb-8">
-            <p className="text-black">We are honored to call this land home and to continue the long-standing tradition of gathering around food, a practice that First </p>
-            <p className="text-black">Nations peoples have nurtured for thousands of years. As we stand on this unceded land, we acknowledge the First Nations peoples as its original custodians and recognize their enduring connection to the land,</p>
-            <p className="text-black">waters, sky, and community. We pay our deepest respects to Elders past, present, and emerging, who carry the wisdom, traditions, and hopes of Aboriginal and Torres Strait Islander peoples. This always was, and always will be, Aboriginal land.</p>
+        <div className="footer border-t-2 border-black dark:border-white w-full text-center leading-8 pt-8 pb-8">
+            <p className="text-black dark:text-gray-400">We are honored to call this land home and to continue the long-standing tradition of gathering around food, a practice that First </p>
+            <p className="text-black dark:text-gray-400">Nations peoples have nurtured for thousands of years. As we stand on this unceded land, we acknowledge the First Nations peoples as its original custodians and recognize their enduring connection to the land,</p>
+            <p className="text-black dark:text-gray-400">waters, sky, and community. We pay our deepest respects to Elders past, present, and emerging, who carry the wisdom, traditions, and hopes of Aboriginal and Torres Strait Islander peoples. This always was, and always will be, Aboriginal land.</p>
         </div>
     );
 };
