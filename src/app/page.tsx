2fa8f9d0f7bf4fd5d4b2583f1088dbffb4f193b3
Harris Alam <harrisalam3@gmail.com>
2025-02-25 16:53:47 +0000

    implement react-animated-cursor

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,7 @@ import HeaderImage from "@/app/HeaderImage";
 import AdvertSection from "@/app/AdvertSection";
 import Footer from "@/app/Footer";
 import Testimonials from "@/app/Testimonials";
-import Lenis from 'lenis';
+import AnimatedCursor from "react-animated-cursor"
 
 export default function Home() {
     const [theme, setTheme] = useState("light");
@@ -20,40 +20,56 @@ export default function Home() {
 
     return (
         <>
-            <div
-                className="hero-section min-h-screen bg-white dark:bg-black text-black dark:text-white transition-colors duration-300 px-4 sm:px-8 md:px-16">
-                {/* Theme Toggle Button */}
-                <button
-                    onClick={() => setTheme(theme === "light" ? "dark" : "light")}
-                    className="absolute top-4 right-4 px-4 py-2 bg-gray-200 dark:bg-white dark:text-black rounded"
-                >
-                    Toggle {theme === "light" ? "Dark" : "Light"} Mode
-                </button>
-
-                {/* First Section with Header */}
-                <div className="mt-16 flex">
-                    <Header/>
-                </div>
-
-                {/* Second Section with Header Image */}
-                <div className="mt-16 flex flex-col items-center">
-                    <HeaderImage/>
-                </div>
-
-                {/* Third Section with Advert Images */}
-                <div className="mt-16 flex flex-col items-center">
-                    <AdvertSection/>
-                </div>
-
-                <div className="testimonials mt-16">
-                    <Testimonials />
-                </div>
-
-                <div className="footer mt-16 flex items-center">
-                    <Footer/>
-                </div>
-
+            <div className="App">
+                <AnimatedCursor
+                    innerSize={8}
+                    outerSize={35}
+                    innerScale={1}
+                    outerScale={2}
+                    outerAlpha={0}
+                    hasBlendMode={true}
+                    innerStyle={{
+                        backgroundColor: '#000'
+                    }}
+                    outerStyle={{
+                        border: '3px solid #000'
+                    }}
+                />
             </div>
+                <div
+                    className="hero-section min-h-screen bg-white dark:bg-black text-black dark:text-white transition-colors duration-300 px-4 sm:px-8 md:px-16">
+                    {/* Theme Toggle Button */}
+                    <button
+                        onClick={() => setTheme(theme === "light" ? "dark" : "light")}
+                        className="absolute top-4 right-4 px-4 py-2 bg-gray-200 dark:bg-white dark:text-black rounded"
+                    >
+                        Toggle {theme === "light" ? "Dark" : "Light"} Mode
+                    </button>
+
+                    {/* First Section with Header */}
+                    <div className="mt-16 flex">
+                        <Header/>
+                    </div>
+
+                    {/* Second Section with Header Image */}
+                    <div className="mt-16 flex flex-col items-center">
+                        <HeaderImage/>
+                    </div>
+
+                    {/* Third Section with Advert Images */}
+                    <div className="mt-16 flex flex-col items-center">
+                        <AdvertSection/>
+                    </div>
+
+                    <div className="testimonials mt-16">
+                        <Testimonials />
+                    </div>
+
+                    <div className="footer mt-16 flex items-center">
+                        <Footer/>
+                    </div>
+
+                </div>
         </>
     );
 }
