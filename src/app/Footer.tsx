339e5f3e0aca7c01538565c7d843ce45d3ac75ac
Harris Alam <harrisalam3@gmail.com>
2025-02-25 16:52:10 +0000

    center text

--- src/app/Footer.tsx
+++ src/app/Footer.tsx
@@ -2,10 +2,10 @@ import React from 'react';
 
 const Footer = () => {
     return (
-        <div className="footer border-t-2 border-black dark:border-white text-center w-full leading-8 pt-8 pb-8 flex justify-center">
-            {/*<p className="container text-black dark:text-gray-400">We are honored to call this land home and to continue the long-standing tradition of gathering around food, a practice that First </p>*/}
-            {/*<p className="container text-black dark:text-gray-400">Nations peoples have nurtured for thousands of years. As we stand on this unceded land, we acknowledge the First Nations peoples as its original custodians and recognize their enduring connection to the land,</p>
-            <p className="container text-black dark:text-gray-400">waters, sky, and community. We pay our deepest respects to Elders past, present, and emerging, who carry the wisdom, traditions, and hopes of Aboriginal and Torres Strait Islander peoples. This always was, and always will be, Aboriginal land.</p>*/}
+        <div className="footer border-t-2 border-gray-300 dark:border-white text-center w-full pt-8 pb-8 m-auto">
+            <p className="container text-black dark:text-gray-400 m-auto text-xs">We are honored to call this land home and to continue the long-standing tradition of gathering around food, a practice that First </p>
+            <p className="container text-black dark:text-gray-400 m-auto text-xs">Nations peoples have nurtured for thousands of years. As we stand on this unceded land, we acknowledge the First Nations peoples as its original custodians and recognize their enduring connection to the land,</p>
+            <p className="container text-black dark:text-gray-400 m-auto text-xs">waters, sky, and community. We pay our deepest respects to Elders past, present, and emerging, who carry the wisdom, traditions, and hopes of Aboriginal and Torres Strait Islander peoples. This always was, and always will be, Aboriginal land.</p>
         </div>
     );
 };
