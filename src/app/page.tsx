6bb009b80a1d210327ad11953f7d627e4b862455
Harris Alam <harrisalam3@gmail.com>
2025-02-27 17:02:41 +0000

    Add slider component to main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,6 +10,7 @@ import AdvertSection from "@/app/AdvertSection";
 import Footer from "@/app/Footer";
 import Testimonials from "@/app/Testimonials";
 import AnimatedCursor from "react-animated-cursor"
+import Slider from "@/app/Slider";
 
 export default function Home() {
     const [theme, setTheme] = useState("light");
@@ -21,7 +22,7 @@ export default function Home() {
     return (
         <>
             <div className="App">
-                <AnimatedCursor
+                {/*<AnimatedCursor
                     innerSize={8}
                     outerSize={35}
                     innerScale={1}
@@ -29,44 +30,48 @@ export default function Home() {
                     outerAlpha={0}
                     hasBlendMode={true}
                     innerStyle={{
-                        backgroundColor: '#000'
+                        backgroundColor: '#e7e7e7'
                     }}
                     outerStyle={{
-                        border: '3px solid #000'
+                        border: '3px solid #e7e7e7'
                     }}
-                />
+                />*/}
             </div>
                 <div
                     className="hero-section min-h-screen bg-white dark:bg-black text-black dark:text-white transition-colors duration-300 px-4 sm:px-8 md:px-16">
                     {/* Theme Toggle Button */}
                     <button
                         onClick={() => setTheme(theme === "light" ? "dark" : "light")}
-                        className="absolute top-4 right-4 px-4 py-2 bg-gray-200 dark:bg-white dark:text-black rounded"
+                        className="flex px-4 py-2 bg-gray-200 dark:bg-white dark:text-black rounded"
                     >
                         Toggle {theme === "light" ? "Dark" : "Light"} Mode
                     </button>
 
                     {/* First Section with Header */}
                     <div className="mt-16 flex">
-                        <Header/>
+                        <Header />
                     </div>
 
                     {/* Second Section with Header Image */}
                     <div className="mt-16 flex flex-col items-center">
-                        <HeaderImage/>
+                        <HeaderImage />
                     </div>
 
                     {/* Third Section with Advert Images */}
                     <div className="mt-16 flex flex-col items-center">
-                        <AdvertSection/>
+                        <AdvertSection />
                     </div>
 
                     <div className="testimonials mt-16">
                         <Testimonials />
                     </div>
 
+                    <div className="slider mt-16">
+                        <Slider />
+                    </div>
+
                     <div className="footer mt-16 flex items-center">
-                        <Footer/>
+                        <Footer />
                     </div>
 
                 </div>
