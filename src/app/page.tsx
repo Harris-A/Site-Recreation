333d4a4a5a2acd6793803ad46d857306f45e443d
Harris Alam <harrisalam3@gmail.com>
2025-03-04 17:16:27 +0000

    Add Callout component to main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,47 +1,47 @@
 // -- Site recreation - https://twogood.com.au/ --
-
-'use client'
-
 import Header from "./Header";
 import HeaderImage from "@/app/HeaderImage";
 import AdvertSection from "@/app/AdvertSection";
 import Footer from "@/app/Footer";
 import Testimonials from "@/app/Testimonials";
+import Callout from "@/app/callout";
 //import AnimatedCursor from "react-animated-cursor"
 import Slider from "@/app/Slider";
 
 export default function Home() {
     return (
-                <div
-                    className="hero-section min-h-screen bg-white dark:bg-[#191919] text-black dark:text-[#f6fef4b0] transition-colors duration-300 px-4 sm:px-8 md:px-16">
+        <div className="hero-section min-h-screen bg-white dark:bg-[#191919] text-black dark:text-[#f6fef4b0] transition-colors duration-300 px-4 sm:px-8 md:px-16">
 
-                    {/* First Section with Header */}
-                    <div className="flex">
-                        <Header />
-                    </div>
+            <div className="flex justify-center">
+                <Callout />
+            </div>
 
-                    {/* Second Section with Header Image */}
-                    <div className="mt-16 flex flex-col items-center">
-                        <HeaderImage />
-                    </div>
+            {/* First Section with Header */}
+            <div className="flex dark:text-[#f6fef4b0]">
+                <Header />
+            </div>
 
-                    {/* Third Section with Advert Images */}
-                    <div className="mt-16 flex flex-col items-center">
-                        <AdvertSection />
-                    </div>
+            {/* Second Section with Header Image */}
+            <div className="mt-16 flex flex-col items-center">
+                <HeaderImage />
+            </div>
 
-                    <div className="testimonials mt-16">
-                        <Testimonials />
-                    </div>
+            {/* Third Section with Advert Images */}
+            <div className="mt-16 flex flex-col items-center">
+                <AdvertSection />
+            </div>
 
-                    <div className="slider">
-                        <Slider />
-                    </div>
+            <div className="testimonials mt-16">
+                <Testimonials />
+            </div>
 
-                    <div className="footer mt-16 flex items-center">
-                        <Footer />
-                    </div>
+            <div className="slider">
+                <Slider />
+            </div>
 
-                </div>
+            <div className="footer mt-16 flex items-center">
+                <Footer />
+            </div>
+        </div>
     );
 }
