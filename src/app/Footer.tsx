515be0ff41b331ed88391ce59fef1e82e68fe0b0
Harris Alam <harrisalam3@gmail.com>
2025-03-04 17:17:27 +0000

    Update Footer Component

--- src/app/Footer.tsx
+++ src/app/Footer.tsx
@@ -1,14 +1,17 @@
-import React from 'react';
-import { Box } from "@radix-ui/themes";
+import { Box, Link} from "@radix-ui/themes";
 
 const Footer = () => {
     return (
         <div className="footer border-t-2 border-gray-300 dark:border-white text-center w-full pt-8 pb-8 m-auto">
 
-            <Box width="64px" height="64px">
-                <li>Test 1</li>
-                <li>Test 1</li>
-                <li>Test 1</li>
+            <Box width="100px" height="100px">
+                <ul>
+                    <li><Link href="#" color="gray">Facebook</Link></li>
+                    <li><Link href="#" color="gray">Instagram</Link></li>
+                    <li><Link href="#" color="gray">Twitter</Link></li>
+                    <li><Link href="#" color="gray">LinkedIn</Link></li>
+                    <li><Link href="#" color="gray">YouTube</Link></li>
+                </ul>
             </Box>
 
             <p className="container text-black dark:text-[#f6fef4b0] m-auto text-xs">We are honored to call this land home and to continue the long-standing tradition of gathering around food, a practice that First </p>
