5b3145d574434848f45b85800a98b2efd0ce4030
Harris Alam <harrisalam3@gmail.com>
2025-03-11 12:37:33 +0000

    Update tooltip to add TooltipProvider

--- src/app/Footer.tsx
+++ src/app/Footer.tsx
@@ -1,5 +1,7 @@
-import {Box, Section, Grid, Heading, Tooltip, Link} from "@radix-ui/themes";
+import {Box, Section, Grid, Heading, Link, IconButton} from "@radix-ui/themes";
+import * as Tooltip from "@radix-ui/react-tooltip";
 import {TwitterLogoIcon, InstagramLogoIcon, LinkedInLogoIcon} from "@radix-ui/react-icons";
+import {TooltipArrow, TooltipPortal} from "@radix-ui/react-tooltip";
 
 const Footer = () => {
     return (
@@ -8,36 +10,60 @@ const Footer = () => {
                 <Grid columns="3" gap="3" width="auto">
                     <div className="footer-social">
                         <Heading size="2" weight="medium" color="gray" className="uppercase text-left">talk to us</Heading>
+
                         <div className="social-icons flex gap-2 mt-2">
-                            <Tooltip content="See us on Twitter">
-                                <Link href="#"><TwitterLogoIcon width="20" height="20" className="hover:text-black dark:hover:text-lime-100" /></Link>
-                            </Tooltip>
-                            <Tooltip content="See us on Instagram">
-                                <Link href="#"><InstagramLogoIcon width="20" height="20" className="hover:text-black dark:hover:text-lime-100" /></Link>
-                            </Tooltip>
-                            <Tooltip content="See us on LinkedIn">
-                                <Link href="#"><LinkedInLogoIcon width="20" height="20" className="hover:text-black dark:hover:text-lime-100"/></Link>
-                            </Tooltip>
+                            <Tooltip.Provider>
+                                <Tooltip.Root>
+                                    <Tooltip.Trigger asChild>
+                                        <IconButton className="IconButton">
+                                            <TwitterLogoIcon width="25" height="25"/>
+                                        </IconButton>
+                                    </Tooltip.Trigger>
+                                    <TooltipPortal>
+                                        <Tooltip.Content className="tooltip-content" sideOffset={5}>
+                                            See us on Twitter
+                                            <Tooltip.Arrow className="tooltip-arrow" />
+                                        </Tooltip.Content>
+                                    </TooltipPortal>
+                                </Tooltip.Root>
+                            </Tooltip.Provider>
+
+                            <Tooltip.Provider>
+                                <Tooltip.Root>
+                                    <Tooltip.Trigger asChild>
+                                        <IconButton className="IconButton">
+                                            <InstagramLogoIcon width="25" height="25"/>
+                                        </IconButton>
+                                    </Tooltip.Trigger>
+                                    <TooltipPortal>
+                                        <Tooltip.Content className="tooltip-content" sideOffset={5}>
+                                            See us on Instagram
+                                            <Tooltip.Arrow className="tooltip-arrow" />
+                                        </Tooltip.Content>
+                                    </TooltipPortal>
+                                </Tooltip.Root>
+                            </Tooltip.Provider>
+
+                            <Tooltip.Provider>
+                                <Tooltip.Root>
+                                    <Tooltip.Trigger asChild>
+                                        <IconButton className="IconButton">
+                                            <LinkedInLogoIcon width="25" height="25" />
+                                        </IconButton>
+                                    </Tooltip.Trigger>
+                                    <TooltipPortal>
+                                        <Tooltip.Content className="tooltip-content" sideOffset={5}>
+                                            See us on LinkedIn
+                                            <Tooltip.Arrow className="tooltip-arrow" />
+                                        </Tooltip.Content>
+                                    </TooltipPortal>
+                                </Tooltip.Root>
+                            </Tooltip.Provider>
                         </div>
                     </div>
                 </Grid>
             </Section>
-            {/*<Section size="1">
-                <Grid columns="1" gap="3" width="auto">
-                    <Heading size="2" weight="medium" color="gray" className="uppercase text-right">talk to us</Heading>
-                    <div className="footer-social flex gap-2">
-                        <Tooltip content="See us on Twitter">
-                            <Link href="#"><TwitterLogoIcon width="20" height="20" className="hover:text-black" /></Link>
-                        </Tooltip>
-                        <Tooltip content="See us on Instagram">
-                            <Link href="#"><InstagramLogoIcon width="20" height="20" className="hover:text-black" /></Link>
-                        </Tooltip>
-                        <Tooltip content="See us on LinkedIn">
-                            <Link href="#"><LinkedInLogoIcon width="20" height="20" className="hover:text-black"/></Link>
-                        </Tooltip>
-                    </div>
-                </Grid>
-            </Section>*/}
+
             <Box py="8" >
                 <Section size="1">
                     <p className="container text-black dark:text-[#f6fef4b0] m-auto text-xs">We are honored to call this land home and to continue the long-standing tradition of gathering around food, a practice that First </p>
