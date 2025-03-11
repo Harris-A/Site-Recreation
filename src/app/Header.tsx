9bcff8a583dbcd8361bb3245fa35344c467fdbd1
Harris Alam <harrisalam3@gmail.com>
2025-03-11 12:38:24 +0000

    Remove Animated heading

--- src/app/Header.tsx
+++ src/app/Header.tsx
@@ -1,37 +1,21 @@
 'use client'
-import { motion } from 'framer-motion';
-import Typical from 'react-typical';
 
+import { motion } from 'framer-motion';
+import { Link } from "@radix-ui/themes";
 export default function Header() {
     return (
         <div>
-            {/* Animated Logo */}
-            <motion.img
+            {/* Animated Logo with link */}
+            <Link href="/">
+                <motion.img
                 initial={{ opacity: 0, y: -20 }}
                 animate={{ opacity: 1, y: 0 }}
                 transition={{ duration: 0.8 }}
                 src="/logo-new.svg"
-                alt="Two Good Co. Logo"
+                alt="New Bold Inc. Logo"
                 className="absolute top-4 left-4 w-[100px] h-[100px] dark:bg-white"
-            />
-
-            {/* Animated Heading */}
-            <motion.h1
-                initial={{ opacity: 0, y: -20 }}
-                animate={{ opacity: 1, y: 0 }}
-                transition={{ duration: 0.8 }}
-                className="mt-[50px] uppercase xs:text-[10rem] sm:text-[8rem] md:text-[4rem] lg:text-[6rem] xl:text-[6rem] 2xl:text-[10rem] text-gray-bold font-bold tracking-[-0.03em] leading-none"
-            >
-                <Typical
-                    steps={[
-                        'R', 100, 'Re', 200, 'Red', 100, 'Rede', 100, 'Redef', 100, 'Redefi', 100, 'Redefin', 100, 'Redefine', 100,
-                        'Redefine ', 100, 'Redefine t', 100, 'Redefine th', 100, 'Redefine the', 100, 'Redefine the ', 100,
-                        'Redefine the w', 100, 'Redefine the wa', 100, 'Redefine the way', 1000
-                    ]}
-                    loop={Infinity}
-                    wrapper="span"
                 />
-            </motion.h1>
+            </Link>
         </div>
     );
 }
