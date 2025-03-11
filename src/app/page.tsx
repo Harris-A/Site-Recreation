9a8d532c2a0221eb4b54d9a0c9437644af2b5b08
Harris Alam <harrisalam3@gmail.com>
2025-03-11 12:42:09 +0000

    remove header and footer components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,22 +10,11 @@ import Nav from "@/app/Nav";
 
 export default function Home() {
     return (
-        <div className="hero-section min-h-screen bg-white dark:bg-[#191919] text-black dark:text-[#f6fef4b0] transition-colors duration-300 px-4 sm:px-8 md:px-16">
-
-            <div className="flex justify-center">
-                <Callout />
-            </div>
-
-            <div className="flex justify-center">
-                <Nav />
-            </div>
-
-            <div className="flex dark:text-[#f6fef4b0]">
-                <Header />
-            </div>
-
-            <div className="mt-16 flex flex-col items-center">
-                <HeaderImage />
+        <>
+            <div className="hero-section min-h-screen text-black dark:text-[#f6fef4b0] transition-colors duration-300 px-4 sm:px-8 md:px-16">
+                <div className="mt-16 flex flex-col items-center">
+                    <HeaderImage />
+                </div>
             </div>
 
             <div className="mt-16 flex flex-col items-center">
@@ -39,10 +28,6 @@ export default function Home() {
             <div className="slider">
                 <Slider />
             </div>
-
-            <div className="footer mt-16 flex items-center">
-                <Footer />
-            </div>
-        </div>
+        </>
     );
 }
