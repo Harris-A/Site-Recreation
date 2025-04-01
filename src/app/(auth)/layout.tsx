e3c0adf994526e0a72745a034e6f436e0678b67b
Harris Alam <harrisalam3@gmail.com>
2025-04-01 17:23:30 +0100

    add a min height to div on login pages

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -2,7 +2,7 @@ export default function AuthLayout({ children }: {
     readonly children: React.ReactNode;
 }) {
     return (
-        <div className="flex justify-center">
+        <div className="flex justify-center items-center min-h-screen">
             {children}
         </div>
     );
