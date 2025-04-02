2b37d07e8d0188c84255eaa046635a1f52cf70d0
Harris Alam <harrisalam3@gmail.com>
2025-04-02 08:50:36 +0100

    amend shop page heading size

--- src/components/custom/ShopLatest.tsx
+++ src/components/custom/ShopLatest.tsx
@@ -3,7 +3,7 @@ import {Heading, Text} from '@radix-ui/themes'
 export default function ShopLatest () {
     return (
         <>
-            <h1 className="tracking-in-expand-fwd-bottom shop-heading px-16 uppercase text-[10rem] text-center leading-none font-extrabold tracking-tighter mb-6">discover the best</h1>
+            <h1 className="tracking-in-expand-fwd-bottom shop-heading px-16 uppercase text-[6rem] text-center leading-none font-extrabold tracking-tighter mb-6 dark:text-[#f6fef4b0]">discover the best</h1>
             <div className="tracking-in-expand-fwd-bottom m-auto flex text-center justify-center mb-6 px-16">
                 <Text size="5" color="gray"><span>We create high-quality, sustainable, and luxurious kitchen essentials—cookware, linens, candles, and more. Everything designed to bring warmth, comfort, and a sense of home to your cooking space. The best part? Every purchase has the power to change a life.</span></Text>
             </div>
