8412309974b94d762b88a752e370bcf45780ef82
Harris Alam <harrisalam3@gmail.com>
2025-05-13 10:36:14 +0100

    Refactor Header component to include Cart and improve layout

--- src/components/custom/Header.tsx
+++ src/components/custom/Header.tsx
@@ -2,20 +2,28 @@
 
 import { motion } from 'framer-motion';
 import { Link } from "@radix-ui/themes";
+import Cart from "@/components/cart/cart";
 export default function Header() {
     return (
-        <div>
-            {/* Animated Logo with link */}
-            <Link href="/">
-                <motion.img
-                initial={{ opacity: 0, y: -20 }}
-                animate={{ opacity: 1, y: 0 }}
-                transition={{ duration: 0.8 }}
-                src="/logo-new.svg"
-                alt="New Bold Inc. Logo"
-                className="absolute top-4 left-4 w-[100px] h-[100px] dark:bg-white"
-                />
-            </Link>
-        </div>
+        <>
+            <header className="w-full px-4 py-3 flex items-center justify-between border-b">
+                <div>
+                {/* Animated Logo with link */}
+                <Link href="/">
+                    <motion.img
+                    initial={{ opacity: 0, y: -20 }}
+                    animate={{ opacity: 1, y: 0 }}
+                    transition={{ duration: 0.8 }}
+                    src="/logo-new.svg"
+                    alt="New Bold Inc. Logo"
+                    className="absolute top-4 left-4 w-[100px] h-[100px] dark:bg-white"
+                    />
+                </Link>
+                </div>
+                <div className="flex items-center gap-4">
+                    <Cart />
+                </div>
+            </header>
+        </>
     );
 }
