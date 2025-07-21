5f3b779ca2a8036a5c76984c025da7bb3a3f9c2f
harris alam <harrisalam3@gmail.com>
2025-07-21 17:24:44 +0100

    create product page for black hoodie

---
+++ src/app/shop/products/Black-Hoodie/page.tsx
@@ -0,0 +1,10 @@
+import ProductDetail from "@/components/shop/Black-Hoodie/ProductDetail";
+export function BlackHoodieProductPage () {
+  return (
+    <div>
+      <ProductDetail />
+    </div>
+  )
+}
+
+export default BlackHoodieProductPage
